1fbc79c4b796e69598bf7751c925cc7a84c0418a
Guiark <lenormand.guil@gmail.com>
2024-11-20 17:05:31 +0100

    ajout

--- PortFolio/Pages/accueil.php
+++ PortFolio/Pages/accueil.php
@@ -2,3 +2,41 @@
 
 require_once("yaml/yaml.php");
 $data=yaml_parse_file('Data/accueil.yaml');
+
+echo "<h1>".$data["titre"]."</h1>\n";
+
+echo "<h2>Sommaire</h2>\n";
+foreach($data["sommaire"]AS $unsommaire){
+    echo "<p>".ucfirst($unsommaire["accueil"]).":".$unsommaire["Compétences"]."".$unsommaire["Réalisations"]."</p>\n";
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+?>

a24a2e35b5fbb4447e4621c79b89bb13f4323864
Guillaume <lenormand.guil@gmail.com>
2024-12-04 15:07:41 +0100

    php

--- Docs/Pages/accueil.php
+++ Docs/Pages/accueil.php
@@ -10,8 +10,15 @@ foreach($data["sommaire"]AS $unsommaire){
     echo "<p>".ucfirst($unsommaire["accueil"]).":".$unsommaire["Compétences"]."".$unsommaire["Réalisations"]."</p>\n";
 }
 
+echo "<h3>Accueil</h3>\n";
+foreach($data["accueil"]As $accueil){
+    echo"<p>".ucfirst($accueil["nom et prénom"]).":".$Accroche["accroche"].":".$Accroche["paragraphe de présentation"].":".$Accroche["photo d'illustration"]."</p>\n";
+}
 
-
+echo "<h3>Compétences</h3>\n";
+foreach($data["compétences"]As $compétences){
+    echo"<p>".ucfirst($compétences["développement"]).":".$compétences["html"].":".$Accroche["Niveau html"].":".$Accroche["css"].":".$compétences["Niveau css"].":".$compétences["php"].":".$Accroche["Niveau php"].":".$Accroche["sql"].":".$Accroche["Niveau sql"].":".$Accroche["sql"]."</p>\n";
+}
 
 
 

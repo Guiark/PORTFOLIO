4c4e7121706df976fed53eff77ec3819e4572d78
Nolanc61 <lenormand.guil@gmail.com>
2024-12-16 11:50:26 +0100

    teste

---
+++ docs/Index-PORFOLIO.php
@@ -0,0 +1,11 @@
+<?php
+
+#require_once '../Docs/Pages/accueil.php'; // On inclut le fichier de configuration (uniquement 1 fois)
+#include '../Docs/Pages/accueil.php';      // On inclut l'en-tête HTML
+#include 'menu.php';        // On inclut un menu de navigation
+
+echo "je teste un truc laisse moi";
+
+#include 'footer.php';      // On inclut le pied de page
+
+
